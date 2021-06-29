4d3554db20c887d38cceb988b03e38ccd73f3076
Laizorrane Teixeira R. P. de Oliveira <85450022+laizorrane@users.noreply.github.com>
2021-06-29 12:30:56 -0300

    Add files via upload
    Primeiro commit do projeto socialbooks

---
+++ socialbooks/src/main/java/com/algaworks/socialbooks/repository/LivrosRepository.java
@@ -0,0 +1,12 @@
+package com.algaworks.socialbooks.repository;
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+
+	import com.algaworks.socialbooks.domain.Livro;
+
+	public interface LivrosRepository  extends JpaRepository<Livro, Long>{
+
+		//Livro findOne(Long id);
+
+	}
+
---
+++ socialbooks/src/main/java/com/algaworks/socialbooks/resources/fromCurrentRequest.java
@@ -0,0 +1,5 @@
+package com.algaworks.socialbooks.resources;
+
+public @interface fromCurrentRequest {
+
+}
